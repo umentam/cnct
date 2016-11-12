dc614f3e5c17e113c170a99a5ff61d2425059426
Anthony Maina <anthony.maina@savana.co.ke>
2016-11-12 13:05:05 -0600

    Fixes to pods

--- cnnct/cnnct/FirstViewController.swift
+++ cnnct/cnnct/FirstViewController.swift
@@ -7,19 +7,30 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class FirstViewController: UIViewController {
-
+    
+    
+    var ref:FIRDatabaseReference!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        ref = FIRDatabase.database().reference()
+        
         // Do any additional setup after loading the view, typically from a nib.
+        self.navigationController?.navigationBar.topItem?.title = "Pick Your Event Interests"
+        self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor(red: 255.0/255.0, green: 87.0/255.0, blue: 34.0/255.0, alpha: 1.0),NSFontAttributeName:UIFont.systemFont(ofSize: 25, weight: UIFontWeightThin)]
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    override func viewDidAppear(_ animated: Bool) {
+        //self.ref.child("Events").childByAutoId().updateChildValues(")
+    }
+    
+    
 }
-
