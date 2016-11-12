d0e0d007e87c94a793e169a87f94051b583f115d
Anthony Maina <anthony.maina@savana.co.ke>
2016-11-11 23:39:30 -0600

    Initial Commit

---
+++ cnnct/cnnct/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  cnnct
+//
+//  Created by Anthony Wamunyu Maina on 11/11/16.
+//  Copyright © 2016 Anthony Wamunyu Maina. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
