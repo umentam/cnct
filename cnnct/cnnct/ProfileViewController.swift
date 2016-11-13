2a769f34795cd75c131ab4def7325e8b692659c7
Gabriel Wamunyu <gabrielwamunyu@Gabriels-Mac-mini.local>
2016-11-13 03:56:49 -0600

    basic user profile

--- cnnct/cnnct/ProfileViewController.swift
+++ cnnct/cnnct/ProfileViewController.swift
@@ -9,9 +9,17 @@
 import UIKit
 import FirebaseAuth
 import FBSDKLoginKit
+import FBSDKCoreKit
+
 
 class ProfileViewController: UIViewController {
-
+    
+    
+    var userName : String = ""
+    @IBOutlet weak var profilePicImageView: UIImageView!
+    
+    @IBOutlet weak var usernameTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -19,8 +27,54 @@ class ProfileViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         self.navigationController?.navigationBar.topItem?.title = "Your Profile"
         self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor(red: 2.0/255.0, green: 208.0/255.0, blue: 172.0/255.0, alpha: 1.0),NSFontAttributeName:UIFont.systemFont(ofSize: 25, weight: UIFontWeightLight)]
+        
+        fetchUserInformation()
     }
-
+    
+    func fetchUserInformation(){
+        
+        if((FBSDKAccessToken.current()) != nil){
+            
+            let graphRequest : FBSDKGraphRequest = FBSDKGraphRequest(graphPath:  "me", parameters: nil)
+            graphRequest.start(completionHandler: { (connection, result, error) -> Void in
+                
+                if ((error) != nil)
+                {
+                    // Process error
+                    print("Error: \(error)")
+                }
+                else
+                {
+                   
+                    
+                    let data:[String:AnyObject] = result as! [String : AnyObject]
+                    self.userName = (data["name"] as? String)!
+                    
+                   
+                    
+                    let userID = data["id"] as! NSString
+                   
+                    let facebookProfileUrl = "https://graph.facebook.com/\(userID)/picture?type=large"
+                    let url = URL(string: facebookProfileUrl)
+                    self.profilePicImageView.layer.cornerRadius = self.profilePicImageView.frame.size.width / 2
+                    self.profilePicImageView.clipsToBounds = true
+                    
+                    if let imageData = NSData(contentsOf: url!) as? Data{
+                        self.profilePicImageView.image = UIImage(data: imageData)
+                    }else{
+                        print("no image data")
+                    }
+                    
+                    self.usernameTextView.text = self.userName
+ 
+                    
+                    //   self.userEmail = result.valueForKey("email") as? String
+                    
+                }
+            })
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
